a251f6d221e15fcb2a5f8692f19f7896e5d4984e
1517643 <1517643@MHIR2015150164.mhir.local>
2017-09-29 20:36:38 +0900

    standalone

---
+++ reversi-ai/src/subClass/Standalone.java
@@ -0,0 +1,72 @@
+package subClass;
+
+import jp.takedarts.reversi.Board;
+import jp.takedarts.reversi.Piece;
+import jp.takedarts.reversi.Position;
+
+public class Standalone {
+
+	public static void main(String[] args) {
+
+		Board testBoard = new Board();
+
+		testBoard.putPiece(3, 3, Piece.BLACK);
+		testBoard.putPiece(4, 4, Piece.BLACK);
+
+		testBoard.putPiece(3, 4, Piece.WHITE);
+		testBoard.putPiece(4, 3, Piece.WHITE);
+				
+		//自分(black)　←ここを更新
+		MinMaxProcessor myProcessor = new MinMaxProcessor();
+		Piece piece = Piece.BLACK;
+
+		//相手(white)　←ここを更新
+		MonteCarloProcessor opponentProcessor = new MonteCarloProcessor();
+		Piece opponentPiece = Piece.WHITE;
+
+		System.out.println("AI(BLACK)(自分)：　" + myProcessor.getName());
+		System.out.println("AI(WHITE)(相手)：　" + opponentProcessor.getName());
+		System.out.println("");
+		
+		
+		System.out.println(testBoard);
+		System.out.println("");
+		
+		
+		while (testBoard.hasEnablePositions(piece) || testBoard.hasEnablePositions(opponentPiece)) {
+
+			//自分の手を置く
+			if (!testBoard.hasEnablePositions(piece)) {
+				continue;
+
+			} else {
+
+				Position myPosition = myProcessor.nextPosition(testBoard, piece, 30000);
+				testBoard.putPiece(myPosition, piece);
+
+				System.out.println(testBoard);
+				System.out.println("");
+
+			}
+			//相手の手を置く
+			if (!testBoard.hasEnablePositions(opponentPiece)) {
+				continue;
+
+			} else {
+
+				Position opponentPosition = opponentProcessor.nextPosition(testBoard, opponentPiece, 30000);
+				testBoard.putPiece(opponentPosition, opponentPiece);
+				System.out.println("");
+
+			}
+
+			System.out.println(testBoard);
+			System.out.println("");
+		}
+		
+		System.out.println("黒の数：　" + testBoard.countPiece(piece));
+		System.out.println("白の数：　" + testBoard.countPiece(opponentPiece));
+		
+	}
+
+}
