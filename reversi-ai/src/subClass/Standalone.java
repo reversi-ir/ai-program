9b692525b243441a7ebd6cbb43278ff452cc23e7
kiyo <kiyo@192.168.1.6>
2017-11-14 23:51:03 +0900

    ２手先を考えるときの評価方法を駒数に変更,log出力

--- reversi-ai/src/subClass/Standalone.java
+++ reversi-ai/src/subClass/Standalone.java
@@ -17,7 +17,7 @@ public class Standalone {
 		testBoard.putPiece(4, 3, Piece.BLACK);
 
 		// 自分(black) ←ここを更新
-		PerceptronProcessor myProcessor = new  PerceptronProcessor();
+		AlphaBetaProcessor myProcessor = new  AlphaBetaProcessor();
 		Piece piece = Piece.BLACK;
 
 		// 相手(white) ←ここを更新
