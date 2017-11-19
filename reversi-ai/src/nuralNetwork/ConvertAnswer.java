6752be4aafce4db0fe03a9468bc29bc4d82dbd6d
Kamata <kamata1015@gmail.com>
2017-11-20 07:38:22 +0900

    14119対戦学習

--- reversi-ai/src/nuralNetwork/ConvertAnswer.java
+++ reversi-ai/src/nuralNetwork/ConvertAnswer.java
@@ -11,7 +11,7 @@ import java.util.List;
 import jp.takedarts.reversi.Board;
 import jp.takedarts.reversi.Piece;
 import jp.takedarts.reversi.Position;
-import subClass.MonteCarloProcessor;
+import subClass.TestMinMaxProcessor;
 
 public class ConvertAnswer {
 
@@ -34,7 +34,7 @@ public class ConvertAnswer {
 			// 教師データの指定
 			// String answerFileName =
 			// "C:/Users/kamat/Desktop/GGFConvert/Othello.latest.278042.ggf.csv";
-			String answerFileName = "C:/Users/kamat/Desktop/GGFConvert/Othello.latest.280844.ggf.csv";
+			String answerFileName = "C:/Users/kamat/Desktop/GGFConvert/Othello.latest.278042.ggf.csv";
 
 			// 教師データ読み込み
 			FileReader fr = new FileReader(answerFileName);
@@ -44,7 +44,7 @@ public class ConvertAnswer {
 			String line;
 			int fileRowNum = 0;
 			FileWriter fw = null;
-			fw = new FileWriter("C:/Users/kamat/Desktop/GGFConvert/teacher_280844_ver2.csv", true);
+			fw = new FileWriter("C:/Users/kamat/Desktop/GGFConvert/teacher_278042.csv", true);
 			// fw = new
 			// FileWriter("C:/Users/kamat/Desktop/GGFConvert/Othello.latest.278042_ver2.csv",
 			// true);
@@ -76,11 +76,11 @@ public class ConvertAnswer {
 					int value = 0;
 
 					// 自分(black) ←ここを更新
-					MonteCarloProcessor myProcessor = new MonteCarloProcessor();
+					TestMinMaxProcessor myProcessor = new TestMinMaxProcessor();
 					Piece piece = Piece.BLACK;
 
 					// 相手(white) ←ここを更新
-					MonteCarloProcessor opponentProcessor = new MonteCarloProcessor();
+					TestMinMaxProcessor opponentProcessor = new TestMinMaxProcessor();
 					Piece opponentPiece = Piece.WHITE;
 
 					Board playBoard = new Board();
