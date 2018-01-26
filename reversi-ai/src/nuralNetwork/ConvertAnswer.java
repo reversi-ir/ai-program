9705db1629808b7cadccbe46f871c185dded8df0
Kamata <kamata1015@gmail.com>
2018-01-26 23:37:07 +0900

    データマージ

--- reversi-ai/src/nuralNetwork/ConvertAnswer.java
+++ reversi-ai/src/nuralNetwork/ConvertAnswer.java
@@ -11,7 +11,7 @@ import java.util.List;
 import jp.takedarts.reversi.Board;
 import jp.takedarts.reversi.Piece;
 import jp.takedarts.reversi.Position;
-import subClass.RandomProcessor;
+import subClass.MinMaxProcessor;
 
 public class ConvertAnswer {
 
@@ -27,13 +27,13 @@ public class ConvertAnswer {
 		List<Integer> yPosition = new ArrayList<Integer>();
 		List<String> color = new ArrayList<String>();
 		List<Double> answer = new ArrayList<Double>();
-		List<Double> draw = new ArrayList<Double>();
 
 		// データの変換
 		try {
 
 			// 教師データの指定
-			// String answerFileName = "C:/Users/kamat/Desktop/GGFConvert/test.ggf.csv";
+			// String answerFileName =
+			// "C:/Users/kamat/Desktop/GGFConvert/Othello.latest.278042.ggf.csv";
 			String answerFileName = "C:/Users/kamat/Desktop/GGFConvert/Othello.latest.278042.ggf.csv";
 
 			// 教師データ読み込み
@@ -44,9 +44,9 @@ public class ConvertAnswer {
 			String line;
 			int fileRowNum = 0;
 			FileWriter fw = null;
-			fw = new FileWriter("C:/Users/kamat/Desktop/GGFConvert/teacher_278042_answer"
-					+ ".csv", true);
-			// fw = new FileWriter("C:/Users/kamat/Desktop/GGFConvert/test_teacher.csv",
+			fw = new FileWriter("C:/Users/kamat/Desktop/GGFConvert/koutou_278042.csv", true);
+			// fw = new
+			// FileWriter("C:/Users/kamat/Desktop/GGFConvert/Othello.latest.278042_ver2.csv",
 			// true);
 			PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
 			while ((line = br.readLine()) != null) {
@@ -72,16 +72,15 @@ public class ConvertAnswer {
 					// 初期盤面の作成
 					Board testBoard = new Board();
 
-					// 評価値
-					double winCount = 0;
-					double drawCount = 0;
+					// 評価値（駒の最終獲得数）
+					int value = 0;
 
 					// 自分(black) ←ここを更新
-					RandomProcessor myProcessor = new RandomProcessor();
+					MinMaxProcessor myProcessor = new MinMaxProcessor();
 					Piece piece = Piece.BLACK;
 
 					// 相手(white) ←ここを更新
-					RandomProcessor opponentProcessor = new RandomProcessor();
+					MinMaxProcessor opponentProcessor = new MinMaxProcessor();
 					Piece opponentPiece = Piece.WHITE;
 
 					Board playBoard = new Board();
@@ -94,134 +93,97 @@ public class ConvertAnswer {
 							testBoard.putPiece(xPosition.get(num), yPosition.get(num), Piece.BLACK);
 							playBoard.putPiece(xPosition.get(num), yPosition.get(num), Piece.BLACK);
 
-							// 1000回プレイアウト
-							for (int s = 0; s < 1000; s++) {
+							while (playBoard.hasEnablePositions(piece) || playBoard.hasEnablePositions(opponentPiece)) {
 
-								while (playBoard.hasEnablePositions(piece)
-										|| playBoard.hasEnablePositions(opponentPiece)) {
+								// 白の手を置く
+								if (playBoard.hasEnablePositions(opponentPiece)) {
+									Position opponentPosition = opponentProcessor.nextPosition(playBoard, opponentPiece,
+											30000);
+									playBoard.putPiece(opponentPosition, opponentPiece);
 
-									if (playBoard.hasEnablePositions(opponentPiece)) {
+								} else if (!playBoard.hasEnablePositions(piece)) {
 
-										Position opponentPosition = opponentProcessor.nextPosition(playBoard,
-												opponentPiece, 30000);
-										playBoard.putPiece(opponentPosition, opponentPiece);
-
-									} else if (!playBoard.hasEnablePositions(opponentPiece)) {
-
-										// TODO パスの際の挙動あれば追記
-
-									}
-
-									// 自分の手を置く
-									if (playBoard.hasEnablePositions(piece)) {
-
-										Position myPosition = myProcessor.nextPosition(playBoard, piece, 30000);
-										playBoard.putPiece(myPosition, piece);
-
-									} else if (!playBoard.hasEnablePositions(piece)) {
-
-										// TODO パスの際の挙動あれば追記
-									}
-
+									// TODO パスの際の挙動あれば追記
 								}
 
-								// 黒が勝った場合
-								if (playBoard.countPiece(Piece.BLACK) > playBoard.countPiece(Piece.WHITE)) {
-									winCount = winCount + 1;
+								if (playBoard.hasEnablePositions(piece)) {
+									Position myPosition = myProcessor.nextPosition(playBoard, piece, 30000);
+									playBoard.putPiece(myPosition, piece);
+
+								} else if (!playBoard.hasEnablePositions(opponentPiece)) {
+
+									// TODO パスの際の挙動あれば追記
 
 								}
-
-								// 引き分けの場合
-								if (playBoard.countPiece(Piece.BLACK) == playBoard.countPiece(Piece.WHITE)) {
-									drawCount = drawCount + 1;
-
-								}
-
-								// 盤面リセット
-								playBoard = new Board(testBoard.getBoard());
 
 							}
 
+							value = playBoard.countPiece(Piece.BLACK) - playBoard.countPiece(Piece.WHITE);
+
+
 							// 評価値の設定
-							answer.set(num, (double) winCount / 1000);
-							draw.add(num, (double) drawCount / 1000);
+							answer.set(num, (double) value);
 							playBoard = new Board(testBoard.getBoard());
-							winCount = 0;
 
 						} else {
 							testBoard.putPiece(xPosition.get(num), yPosition.get(num), Piece.WHITE);
 							playBoard.putPiece(xPosition.get(num), yPosition.get(num), Piece.WHITE);
 
-							// 1000回プレイアウト
-							for (int s = 0; s < 1000; s++) {
+							while (playBoard.hasEnablePositions(piece) || playBoard.hasEnablePositions(opponentPiece)) {
 
-								while (playBoard.hasEnablePositions(piece)
-										|| playBoard.hasEnablePositions(opponentPiece)) {
+								// 黒の手を置く
+								if (playBoard.hasEnablePositions(piece)) {
 
-									// 自分の手を置く
-									if (playBoard.hasEnablePositions(piece)) {
+									Position myPosition = myProcessor.nextPosition(playBoard, piece, 30000);
+									playBoard.putPiece(myPosition, piece);
 
-										Position myPosition = myProcessor.nextPosition(playBoard, piece, 30000);
-										playBoard.putPiece(myPosition, piece);
+								} else if (!playBoard.hasEnablePositions(piece)) {
 
-									} else if (!playBoard.hasEnablePositions(piece)) {
-
-										// TODO パスの際の挙動あれば追記
-
-									}
-
-									if (playBoard.hasEnablePositions(opponentPiece)) {
-
-										Position opponentPosition = opponentProcessor.nextPosition(playBoard,
-												opponentPiece, 30000);
-										playBoard.putPiece(opponentPosition, opponentPiece);
-
-									} else if (!playBoard.hasEnablePositions(opponentPiece)) {
-
-										// TODO パスの際の挙動あれば追記
-
-									}
-
+									// TODO パスの際の挙動あれば追記
 								}
 
-								// 黒が勝った場合
-								if (playBoard.countPiece(Piece.BLACK) > playBoard.countPiece(Piece.WHITE)) {
-									winCount = winCount + 1;
+								if (playBoard.hasEnablePositions(opponentPiece)) {
+
+									Position opponentPosition = opponentProcessor.nextPosition(playBoard, opponentPiece,
+											30000);
+									playBoard.putPiece(opponentPosition, opponentPiece);
+
+								} else if (!playBoard.hasEnablePositions(opponentPiece)) {
+
+									// TODO パスの際の挙動あれば追記
 
 								}
-
-								// 引き分けの場合
-								if (playBoard.countPiece(Piece.BLACK) == playBoard.countPiece(Piece.WHITE)) {
-									drawCount = drawCount + 1;
-
-								}
-
-								// 盤面リセット
-								playBoard = new Board(testBoard.getBoard());
 
 							}
 
+							value = playBoard.countPiece(Piece.BLACK) - playBoard.countPiece(Piece.WHITE);
+
 							// 評価値の設定
-							answer.set(num, (double) winCount / 1000);
-							draw.add(num, (double) drawCount / 1000);
+							answer.set(num, (double) value);
 							playBoard = new Board(testBoard.getBoard());
-							winCount = 0;
+							;
 						}
 
 					}
 
+					// 評価値の設定
+					// value = testBoard.countPiece(Piece.BLACK) -
+					// testBoard.countPiece(Piece.WHITE);
+
 					// 配列の出力
 					for (int outNum = 0; outNum < answer.size(); outNum++) {
+
 
 						pw.print(color.get(outNum) + ",");
 						pw.print(xPosition.get(outNum) + ",");
 						pw.print(yPosition.get(outNum) + ",");
-						pw.print(answer.get(outNum) + ",");
 
-						if (outNum < draw.size() - 1) {
-							pw.print(draw.get(outNum) + ",");
+						if (outNum < answer.size() - 1) {
+							// pw.print(value + ",");
+							pw.print(answer.get(outNum) + ",");
 						} else {
-							pw.print(draw.get(outNum));
+							// pw.print(value);
+							pw.print(answer.get(outNum));
 						}
 					}
 					pw.println("");
@@ -231,7 +193,7 @@ public class ConvertAnswer {
 				yPosition = new ArrayList<Integer>();
 				color = new ArrayList<String>();
 				answer = new ArrayList<Double>();
-				draw = new ArrayList<Double>();
+
 
 			}
 
@@ -241,9 +203,7 @@ public class ConvertAnswer {
 			// 読み込み終了
 			br.close();
 
-		} catch (
-
-		Exception e) {
+		} catch (Exception e) {
 
 			e.printStackTrace();
 		}
--- reversi-ai/src/subClass/AIProcessor.java
+++ reversi-ai/src/subClass/AIProcessor.java
@@ -1,21 +1,19 @@
 package subClass;
 
-import java.util.ArrayList;
-
 import jp.takedarts.reversi.Board;
 import jp.takedarts.reversi.Piece;
 import jp.takedarts.reversi.Position;
 import jp.takedarts.reversi.Processor;
+import nuralNetwork.ReversiPerceptron;
 
-/**
- * Reversi人工知能のサンプルプログラム。
- *
- * @author Nakanishi
- */
 public class AIProcessor extends Processor {
 
-	AlphaBeta alphaBeta = new AlphaBeta();
-	AlphaBetaLylarTwo alphaBetaLylarTwo = new AlphaBetaLylarTwo();
+	// コンストラクタ
+	ReversiPerceptron reversiPerceptron;
+
+	public AIProcessor() {
+		this.reversiPerceptron = new ReversiPerceptron();
+	}
 
 	/**
 	 * 手番が来たときに、次の手を決定するメソッド。
@@ -30,149 +28,42 @@ public class AIProcessor extends Processor {
 	 */
 	@Override
 	public Position nextPosition(Board board, Piece piece, long thinkingTime) {
-
+		// 次に置ける場所の中で、もっとも評価の高い場所を探す
+		double value = -100;
+		double max = -100;
 		int x = -1;
 		int y = -1;
-		double arg = -1000;
-		Bean bean = null;
-		double finalValue = -1000;
-		boolean lastWord = false;
-		boolean enemyCantPut = true;
 
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
+
 				// 置けるかどうかを確認し、置けないのなら何もしない
 				if (!board.isEnablePosition(i, j, piece)) {
 					continue;
 				}
 
-				// 候補手をコンソールに出力
-				System.out.println(i + "," + j);
+				// 同じ盤面を表すオブジェクトを作成し、自分の駒を置く
+				Board next_board = new Board(board.getBoard());
 
-				// 残りの置けるマス目が５マス以下の場合
-				if (board.countPiece(piece) + board.countPiece(Piece.opposite(piece)) >= 60) {
+				next_board.putPiece(i, j, piece);
 
-					// 残りの置けるマス目が２マス以下の場合
-					if (board.countPiece(piece) + board.countPiece(Piece.opposite(piece)) >= 62) {
+				value = reversiPerceptron.ReversiPerceptronCreate(next_board, piece);
 
-						Board next_board = new Board(board.getBoard());
-
-						next_board.putPiece(i, j, piece);
-
-						Board next_board1 = new Board(next_board.getBoard());
-
-						for (int k = 0; k < 8; k++) {
-							for (int l = 0; l < 8; l++) {
-								if (!next_board1.isEnablePosition(k, l, Piece.opposite(piece))) {
-									continue;
-								}
-
-								Board next_board2 = new Board(next_board.getBoard());
-
-								next_board2.putPiece(k, l, Piece.opposite(piece));
-
-								enemyCantPut = false;
-
-								Board next_board3 = new Board(next_board2.getBoard());
-
-								int tempMaxTotalCount = 64 - next_board3.countPiece(Piece.opposite(piece));
-
-								if (finalValue < tempMaxTotalCount) {
-									finalValue = tempMaxTotalCount;
-									x = i;
-									y = j;
-								}
-							}
-						}
-
-						if (enemyCantPut) {
-
-							Board next_board2 = new Board(next_board1.getBoard());
-
-							// next_board2.putPiece(i, j, piece);
-
-							Board next_board3 = new Board(next_board2.getBoard());
-							if (next_board3.countPiece(piece) + next_board3.countPiece(Piece.opposite(piece)) == 64) {
-								finalValue = 64 - next_board3.countPiece(Piece.opposite(piece));
-							} else {
-								finalValue = 63 - next_board3.countPiece(Piece.opposite(piece));
-							}
-							x = i;
-							y = j;
-						}
-
-						// 最後の手だったらlogの文言を「評価値」から「駒数」に変える。
-						lastWord = true;
-					} else {
-
-						// 同じ盤面を表すオブジェクトを作成し、自分の駒を置く(１手目)
-						Board next_board = new Board(board.getBoard());
-
-						next_board.putPiece(i, j, piece);
-
-						// 駒を置いた後の盤面に、さらに相手が駒を置いた場合の最大評価値を計算する
-						bean = alphaBetaLylarTwo._getMaxValue(next_board, piece);
-
-						System.out.println(bean.getPervalueSecond());
-						log(String.format("(%d, %d)に置いた時の評価値は%f", i, j, finalValue));
-
-						// 求めた盤面の最小評価値が最大となる駒の置き場所を保存する
-						if (arg > finalValue) {
-							x = i;
-							y = j;
-
-							finalValue = arg;
-						}
-
-					}
-
-				} else {
-					// 同じ盤面を表すオブジェクトを作成し、自分の駒を置く(１手目)
-					Board next_board = new Board(board.getBoard());
-
-					next_board.putPiece(i, j, piece);
-
-					// 駒を置いた後の盤面に、さらに相手が駒を置いた場合の最大評価値を計算する
-					bean = alphaBeta._getMaxValue(next_board, piece);
-					arg = bean.getPervalueSecond();
-					ArrayList<String> messagesList = bean.getMessagesList();
-
-					for (String messages : messagesList) {
-						log(messages);
-					}
-
-					if (arg == -100) {
-						log(String.format("(%d, %d)時は自分がPASSする可能性の高いworstな手)", i, j));
-					} else if (arg == 100) {
-						log(String.format("(%d, %d)時は相手がPASSする可能性の高いbestな手)", i, j));
-					} else {
-
-						System.out.println(arg);
-						log(String.format("(%d, %d)に置いた時の評価値は%f", i, j, finalValue));
-					}
-					// 求めた盤面の最小評価値が最大となる駒の置き場所を保存する
-					if (arg > finalValue) {
-						x = i;
-						y = j;
-
-						finalValue = arg;
-
-					}
+				// 求めた盤面の最小評価値が最大となる駒の置き場所を保存する
+				if (value > max) {
+					max = value;
+					x = i;
+					y = j;
 				}
-
 			}
 		}
 
-		if (lastWord) {
-			System.out.println("最多駒数は" + (int) finalValue);
-			log(String.format("next -> (%d, %d) : 最多駒数%d", x, y, (int) finalValue));
-		} else {
-			System.out.println("最終評価値は" + finalValue);
-			log(String.format("next -> (%d, %d) : 最高評価値%f", x, y, finalValue));
-		}
+		// 置く場所をログに出力
+		log(String.format("next -> (%d, %d) : %f", x, y, max));
+		// ystem.out.println("評価値：" + max);
 
+		// 置く場所をPositionオブジェクトに変換して返す
 		return new Position(x, y);
-
 	}
 
 	/**
@@ -182,6 +73,7 @@ public class AIProcessor extends Processor {
 	 */
 	@Override
 	public String getName() {
-		return "AlphaBeta+ニューラルネットワーク";
+		return "ニューラルネットワーク";
 	}
+
 }
